2bd77d6a1c0ec8a5b09242a306d4adc7fd669bdd
Jan Martin Reckel (IAI) <jan.reckel@kit.edu>
2025-11-06 14:18:21 +0100

    Implement Web Request and JSON parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-/// Operaton Task worker is a program that periodically fetches open service tasks from the
+/// Operaton Task Worker is a program that periodically fetches open service tasks from the
 /// Operaton instance at Energy Lab and then will try to solve them if any function or sub program
 /// is available.
 
 mod structures;
 
+use std::error::Error;
 use config::Config;
 use structures::ConfigParams;
 
-use log::{debug, error, warn, log_enabled, info, Level};
+use log::{debug, error, warn, log_enabled, info, Level, trace};
+use crate::structures::ServiceTask;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // Get the parameters from the environment variables
     let config = load_config();
 
@@ -25,17 +28,48 @@ fn main() {
 
     loop {
         info!("Test");
-        main_loop(config);
+        match get_open_service_tasks(config).await {
+            Ok(service_tasks) => {
+                trace!(
+                    "We received {} open Service Tasks from Operaton.",
+                    service_tasks.len()
+                )
+            },
+            Err(error) => error!("We were unable to receive and parse any Service Tasks")
+        }
         break;
     }
 }
 
-fn main_loop(config: ConfigParams) {
-    let mut url = config.url().clone();
-    url.set_path("engine-rest/external-task");
-    info!("Fetch data at {}", url.to_string());
+async fn get_open_service_tasks(config: ConfigParams) -> Result<Vec<ServiceTask>, Box<dyn Error>> {
+    let mut service_tasks_endpoint = config.url().clone();
+    service_tasks_endpoint.set_path("engine-rest/external-task");
+    info!("Fetch data at {}", service_tasks_endpoint.to_string());
 
-    
+    match reqwest::get(service_tasks_endpoint.clone()).await {
+        Ok(response) => {
+            match response.json().await {
+                Ok(unwrapped_json) => {
+                    let service_tasks: Vec<ServiceTask> = unwrapped_json;
+                    trace!("Parsed: {:#?}", service_tasks);
+                    Ok(service_tasks)
+                },
+                Err(err) => {
+                    error!("An error occurred while parsing the JSON: {:#?}",
+                    err);
+                    Err(err.into())
+                }
+            }
+        },
+        Err(err) => {
+            error!(
+                "Error while calling API endpoint '{}': {:#?}",
+                service_tasks_endpoint.to_string(),
+                err
+            );
+            Err(err.into())
+        }
+    }
 }
 
 /// Loads the configuration into a [ConfigParams] struct. The function may panic, but it should not
